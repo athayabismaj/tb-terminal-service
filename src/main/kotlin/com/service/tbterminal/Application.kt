910e25e650ab8d7981e14b2c3198639bc47e1d23
athayabismaj <athayabj@gmail.com>
2026-05-24 00:01:29 +0700

    fix(dev): bind ktor to all interfaces

--- src/main/kotlin/com/service/tbterminal/Application.kt
+++ src/main/kotlin/com/service/tbterminal/Application.kt
@@ -4,5 +4,11 @@ import io.ktor.server.engine.*
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
+    // Force binding to 0.0.0.0 so the Android emulator (10.0.2.2) can connect
+    val customArgs = args.toMutableList()
+    if (!customArgs.contains("-host")) {
+        customArgs.add("-host")
+        customArgs.add("0.0.0.0")
+    }
+    io.ktor.server.netty.EngineMain.main(customArgs.toTypedArray())
 }
